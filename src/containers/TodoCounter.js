47136ff241ceef6276dfd8c8cc5846b71fca7c56
Ricardo Lopes <raicuparta@gmail.com>
2017-05-02 13:40:49 +0100

    Counter style cleanup

--- src/containers/TodoCounter.js
+++ src/containers/TodoCounter.js
@@ -1,15 +1,17 @@
+import React from 'react'
 import { connect } from 'react-redux'
-import MenuItem from '../components/MenuItem'
+import { Label, NavItem } from 'react-bootstrap'
 
-const mapStateToProps = (state) => {
-  return {
-    name: 'Total: ' + state.todos.length,
-    path: 'todos'
-  }
-}
+const Counter = ({ label }) => (
+  <NavItem disabled>{label}</NavItem>
+)
+
+const mapStateToProps = (state) => ({
+  label: state.todos.length
+})
 
 const TodoCounter = connect(
   mapStateToProps
-)(MenuItem)
+)(Counter)
 
 export default TodoCounter
