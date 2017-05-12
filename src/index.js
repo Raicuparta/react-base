78a38f71f6c199e8b898458682c7036ac777898e
Ricardo Lopes <raicuparta@gmail.com>
2017-05-12 16:39:32 +0100

    restrict state alterations to explicit actions only

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { useStrict } from 'mobx';
 
 import 'bootstrap/dist/css/bootstrap.css'
 
 import App from './components/App'
 import './index.css'
+
+useStrict(true);
 
 ReactDOM.render(<App /> , document.getElementById('root'))
