e013e63aeeeeadb9e9445dd35c50cfdda8c5fa45
Ricardo Lopes <raicuparta@gmail.com>
2017-05-12 14:59:01 +0100

    using computed instead of auto run for visible todos

--- src/stores/TodoStore.js
+++ src/stores/TodoStore.js
@@ -1,31 +1,22 @@
-import { observable, action, autorun } from 'mobx'
+import { observable, action, computed } from 'mobx'
 
 class TodoStore {
 
   @observable filter = 'SHOW_ALL'
   @observable todos
-  @observable visibleTodos
 
   constructor() {
     this.todos = [{text: 'do it now', completed: false}]
   }
 
-  updateVisibleTodos = autorun(() => {
+  @computed get visibleTodos() {
     if (this.filter === 'SHOW_ACTIVE')
-      this.visibleTodos = this.todos.filter((e) => !e.completed)
+      return observable(this.todos.filter((e) => !e.completed))
     else if (this.filter === 'SHOW_COMPLETED')
-      this.visibleTodos = this.todos.filter((e) => e.completed)
-    else this.visibleTodos = this.todos
-  })
-
-  // @computed get visibleTodos() {
-  //   if (this.filter === 'SHOW_ACTIVE')
-  //     return this.todos.filter((e) => !e.completed)
-  //   else if (this.filter === 'SHOW_COMPLETED')
-  //     return this.todos.filter((e) => e.completed)
-  //   else
-  //     return this.todos
-  // }
+      return observable(this.todos.filter((e) => e.completed))
+    else
+      return this.todos
+  }
 
   @action add = (text) => {
     this.todos.push({text: text, completed: false})
